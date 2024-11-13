aff0aca094a79bc93e106ea281bf8d2b78ecc283
Maxwell Maslov <maslov.maxwell@students.greenriver.edu>
2024-11-13 10:51:39 -0800

    Added node syntax to run the website as a local host

--- src/index.js
+++ src/index.js
@@ -4,9 +4,26 @@ import "./styles.css";
 
 import App from "./App";
 
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
 const root = createRoot(document.getElementById("root"));
 root.render(
     <StrictMode>
         <App />
     </StrictMode>
 );
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`)
+});
+
